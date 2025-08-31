cb56ae5a4538ee6cc5b855da9d27cb09cfb8b61c
SunilLimit <80940775+SunilLimit@users.noreply.github.com>
2025-08-31 07:45:03 +0530

    Final

---
+++ NetSutra/NetSutra/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetSutra
+//
+//  Created by Sunil on 31/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NetSutra/NetSutraTests/NetSutraTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetSutraTests.swift
+//  NetSutraTests
+//
+//  Created by Sunil on 31/08/25.
+//
+
+import Testing
+@testable import NetSutra
+
+struct NetSutraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
